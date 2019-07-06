bbe137a81915870a854ca10ccfa0c7fa383b87c8
Luka Naglić <lu.naglic@gmail.com>
2019-07-06 19:06:06 +0200

    Gotova funkcionalnost interesa. Spremno za spajanje s masterom.

--- login/check_users_description.php
+++ login/check_users_description.php
@@ -5,14 +5,39 @@ if(!isset($_SESSION)) {
 require '../database/db.php';
 $email = $_SESSION['email'];
 
+
+$sql = "SELECT id FROM Korisnik where email='$email'";
+$id = $mysqli->query($sql)->fetch_object()->id;
 //u ovoj datoteci spremamo podatke koji opisuju korisnikove interese
 //tip odnosa, spol, hobiji, sport,...
-
-if(isset($_POST['trazim_spol']))
-{
-  $spol = $_POST['trazim_spol'];
-  $sql = "UPDATE Korisnik SET spol='". $spol ."' WHERE email='$email'";
-  $insert = $mysqli->query($sql);
+if(isset($_POST['btn_opis_korisnika'])){
+  $sql = "DELETE FROM nudim_interese WHERE id = $id";
+  $isprazni = $mysqli->query($sql);
 }
 
+if(isset($_POST['btn_sto_korisnik_trazi'])){
+  $sql = "DELETE FROM trazim_interese WHERE id = $id";
+  $isprazni = $mysqli->query($sql);
+}
+
+function ubaci_u_bazu($cbox, $tablica, $mysqli, $id){
+  if(isset($_POST["$cbox"])){
+    $polje = $_POST["$cbox"];
+    foreach($polje as $vrijednost){
+      $sql = "INSERT INTO $tablica VALUES ($id, '$vrijednost')";
+      $insert = $mysqli->query($sql);
+    }
+  }
+}
+
+
+ubaci_u_bazu('odnos', 'nudim_interese', $mysqli, $id);
+ubaci_u_bazu('hobiji', 'nudim_interese', $mysqli, $id);
+ubaci_u_bazu('odnos2', 'trazim_interese', $mysqli, $id);
+ubaci_u_bazu('hobiji2', 'trazim_interese', $mysqli, $id);
+ubaci_u_bazu('trazim_spol', 'trazim_interese', $mysqli, $id);
+
  ?>
+
+
+
