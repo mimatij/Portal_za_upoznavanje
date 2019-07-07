06b8a978545bb70a0fc35f2ca29ebbae9368ed2e
miatadic <miaa.tadic@gmail.com>
2019-07-07 16:05:41 +0200

    Popravljen logout - praznjenje sessiona

--- login/logout.php
+++ login/logout.php
@@ -1,4 +1,14 @@
 <?php
+    $_SESSION['email'] = '';
+    $_SESSION['registriran'] = '';
+    $_SESSION['ulogiran'] = '';
+    $_SESSION['message'] = '';
+    $_SESSION['ime'] = '';
+
+    $_SESSION['spol'] = '';
+    $_SESSION['prezime'] = '';
+    $_SESSION['grad'] = '';
+
     session_destroy();
 
     header('location: index.php');
